49443836fecbf90f4fc52ce6d60f7b3a2ab3a16a
추창만 <chuchangman@gmail.com>
2026-05-31 17:45:39 +0900

    feat: 숏폼 및 좋아요 페이지 기본 구조 구현

--- frontend/src/views/LikedView.vue
+++ frontend/src/views/LikedView.vue
@@ -1,3 +1,31 @@
+<script setup>
+import mockVideos from '@/data/mockVideos'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const likedVideos = ref(mockVideos.filter((video) => video.isLiked))
+
+const goShortsPage = () => {
+  router.push('/shorts/')
+}
+</script>
+
 <template>
   <h1>좋아요페이지</h1>
+  <h3>좋아요한 영상 후기만 볼 수 있습니다.</h3>
+  <p v-if="likedVideos.length === 0">아직 좋아요한 영상이 없습니다.</p>
+  <ul v-else>
+    <li v-for="likedVideo in likedVideos" :key="likedVideo.id">
+      <div @click="goShortsPage">
+        <img :src="likedVideo.thumbnailUrl" :alt="likedVideo.productName" />
+        <p>{{ likedVideo.productName }}</p>
+        <p>{{ likedVideo.productBrand }}</p>
+        <p>{{ likedVideo.rating }}</p>
+        <p>{{ likedVideo.userNickname }}</p>
+        <p>{{ likedVideo.reviewContent }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
+<style scoped></style>
